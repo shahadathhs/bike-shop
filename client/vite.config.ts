1f3a18a7e2b98f5a748a0e92535ef45b5945b18c
shahadathhs <shahadathhossensajib732@gmail.com>
2025-02-23 04:29:32 +0600

    chore: deployed to vercel

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,16 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild, command }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: "./server/app.ts",
+        }
+      : undefined,
+  },
+  ssr: {
+    noExternal: command === "build" ? true : undefined,
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+}));
