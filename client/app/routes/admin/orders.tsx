a7d02d49e3018fa36b9670f9ab29df486296a7e2
shahadathhs <shahadathhossensajib732@gmail.com>
2025-05-06 07:15:18 +0600

    ui: order data loader in table complonent

--- client/app/routes/admin/orders.tsx
+++ client/app/routes/admin/orders.tsx
@@ -1,13 +1,86 @@
-import { useEffect, useState } from 'react'
-import toast from 'react-hot-toast'
-import { useFetcher } from 'react-router'
-import { getToken } from '~/utils/getToken'
+import { ArrowBigLeft, ArrowBigRight } from 'lucide-react'
+import {
+  redirect,
+  useFetcher,
+  useLoaderData,
+  useNavigate,
+  useSearchParams,
+  type ActionFunction,
+  type LoaderFunction,
+} from 'react-router'
+import { Badge } from '~/components/ui/badge'
+import { Button } from '~/components/ui/button'
+import { Input } from '~/components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '~/components/ui/select'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '~/components/ui/table'
+import { orderStatus } from '~/constant/order'
+import { getCookie } from '~/services/auth.services'
+import type { TCookie } from '~/types/user'
 
-export async function action({ request }: { request: Request }) {
+export const loader: LoaderFunction = async ({ request }) => {
+  const cookie = await getCookie(request)
+
+  if (!cookie.token) return redirect('/login')
+
+  const url = new URL(request.url)
+  const page = Number(url.searchParams.get('page') ?? 1)
+  const limit = Number(url.searchParams.get('limit') ?? 6)
+  const status = url.searchParams.get('status') ?? ''
+  const searchTerm = url.searchParams.get('searchTerm') ?? ''
+
+  const queryParams = new URLSearchParams()
+  queryParams.append('page', String(page))
+  queryParams.append('limit', String(limit))
+  if (status) queryParams.append('status', status)
+  if (searchTerm) queryParams.append('searchTerm', searchTerm)
+
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/orders/admin/getAll?${queryParams.toString()}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${cookie.token}`,
+        },
+      },
+    )
+
+    const { data } = await response.json()
+    console.log('data', data)
+    return {
+      orders: data.orders,
+      metadata: data.metadata,
+      cookie,
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    console.error('Error fetching orders:', err)
+    return { orders: [], metadata: { total: 0, page, limit }, cookie }
+  }
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const cookie = await getCookie(request)
+
+  if (!cookie.token) return redirect('/login')
+  const token = cookie.token
+
   const formData = await request.formData()
-
   const action = formData.get('action') as string
-  const token = formData.get('token') as string
 
   if (action === 'delete') {
     const orderId = formData.get('orderId') as string
@@ -34,8 +107,9 @@ export async function action({ request }: { request: Request }) {
           errorDetails: errorData,
         }
       }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
-      console.error(err)
+      console.error('Error deleting order:', err)
       return {
         error: err.message || 'Failed to delete order',
       }
@@ -68,8 +142,9 @@ export async function action({ request }: { request: Request }) {
           errorDetails: errorData,
         }
       }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
-      console.error(err)
+      console.error('Error marking order as delivered:', err)
       return {
         error: err.message || 'Failed to mark order as delivered',
       }
@@ -100,8 +175,9 @@ export async function action({ request }: { request: Request }) {
           errorDetails: errorData,
         }
       }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
-      console.error(err)
+      console.error('Error canceling order:', err)
       return {
         error: err.message || 'Failed to cancel order',
       }
@@ -111,115 +187,114 @@ export async function action({ request }: { request: Request }) {
   return null
 }
 
+interface OrderRow {
+  _id: string
+  product: { name: string }
+  createdAt: string
+  status: string
+  totalPrice: number
+  isDeleted: boolean
+}
+
+interface LoaderData {
+  orders: OrderRow[]
+  metadata: { total: number; page: number; limit: number }
+  cookie: TCookie
+}
+
 export default function Orders() {
-  const [orders, setOrders] = useState([])
-  const [page, setPage] = useState(1)
-  const [limit, setLimit] = useState(10)
-  const [total, setTotal] = useState(0)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-  const [searchTerm, setSearchTerm] = useState('')
+  const { orders, metadata } = useLoaderData<LoaderData>()
+  const [searchParams] = useSearchParams()
+  const navigate = useNavigate()
+
+  const page = metadata.page
+  const limit = metadata.limit
+  const totalPages = Math.ceil(metadata.total / limit)
+  const currentStatus = searchParams.get('status') ?? ''
+  const searchTerm = searchParams.get('searchTerm') ?? ''
 
   const fetcher = useFetcher()
 
-  const token = getToken()
-
-  // Fetch orders with pagination and optional filtering.
-  const fetchOrders = async (page = 1, limit = 10, search = '') => {
-    setLoading(true)
-    try {
-      const queryParams = new URLSearchParams({
-        page: page.toString(),
-        limit: limit.toString(),
-      })
-      if (search) {
-        queryParams.append('searchTerm', search)
-      }
-
-      const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/orders/admin/getAll?${queryParams.toString()}`,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      )
-      if (!response.ok) {
-        const errData = await response.json()
-        throw new Error(errData.error || 'Failed to fetch orders')
-      }
-      const responseData = await response.json()
-      setOrders(responseData.data.orders)
-      setTotal(responseData.data.metadata.total)
-    } catch (err: any) {
-      console.error(err)
-      setError(err.message || 'Failed to fetch orders')
-      toast.error(err.message || 'Failed to fetch orders')
-    }
-    setLoading(false)
+  // helper to build new URLSearchParams string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function go(params: Record<string, any>) {
+    const p = new URLSearchParams(searchParams)
+    Object.entries(params).forEach(([k, v]) => {
+      if (v == null || v === '') p.delete(k)
+      else p.set(k, String(v))
+    })
+    navigate(`?${p.toString()}`)
   }
-
-  useEffect(() => {
-    if (token) {
-      fetchOrders(page, limit, searchTerm)
-    }
-  }, [token, page, limit, searchTerm, fetcher.state])
-
-  const totalPages = Math.ceil(total / limit)
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Orders</h1>
 
-      {/* Search Input */}
-      <div className="mb-4">
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={e => {
-            setPage(1)
-            setSearchTerm(e.target.value)
-          }}
-          placeholder="Search orders by name, category, or brand"
-          className="input input-bordered max-w--xl"
-        />
+      {/* Search & Status Filter */}
+      <div className="flex flex-wrap gap-4 items-center mb-4">
+        {/* Search Input */}
+        <div>
+          <Input
+            type="text"
+            value={searchTerm}
+            onChange={e => {
+              go({ searchTerm: e.target.value })
+            }}
+            placeholder="Search orders by name, category, or brand"
+          />
+        </div>
+        {/* Status Filter */}
+        <Select
+          value={currentStatus}
+          onValueChange={v => go({ status: v === 'all' ? undefined : v, page: 1 })}
+        >
+          <SelectTrigger className="w-48">
+            <SelectValue placeholder="Filter by status" />
+          </SelectTrigger>
+          <SelectContent>
+            {orderStatus.map(s => (
+              <SelectItem key={s.value} value={s.value}>
+                {s.label}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
       </div>
 
-      {loading && <p>Loading orders...</p>}
-      {error && <p className="text-red-500">{error}</p>}
-      {!loading && orders.length === 0 && <p>No orders found.</p>}
-      {!loading && orders.length > 0 && (
-        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
-          <table className="w-full table table-zebra">
-            <thead className="bg-gray-200">
-              <tr>
-                <th>#</th>
-                <th>Order ID</th>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Status</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders?.map((order: any, index: number) => (
-                <tr key={order._id}>
-                  <td>{index + 1}</td>
-                  <td>{order._id}</td>
-                  <td>{order?.product?.name}</td>
-                  <td>{order?.totalPrice}</td>
-                  <td>{order?.status}</td>
-                  <td className="flex gap-2">
+      {/* Order Table */}
+      <div className="overflow-x-auto border rounded">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Product Name</TableHead>
+              <TableHead>Order ID</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {orders.length > 0 ? (
+              orders.map(order => (
+                <TableRow key={order._id}>
+                  <TableCell>{order.product.name}</TableCell>
+                  <TableCell>{order._id}</TableCell>
+                  <TableCell>{new Date(order.createdAt).toLocaleDateString('en-US')}</TableCell>
+                  <TableCell>
+                    <Badge variant="outline">{order.status}</Badge>
+                  </TableCell>
+                  <TableCell>${order.totalPrice.toFixed(2)}</TableCell>
+                  <TableCell>
                     {/* mark as delivered */}
                     {order?.status !== 'delivered' && !order?.isDeleted && (
                       <fetcher.Form method="patch">
                         <input type="hidden" name="orderId" value={order._id} />
                         <input type="hidden" name="action" value="markAsDelivered" />
-                        <input type="hidden" name="token" value={token as string} />
-                        <button type="submit" className="btn btn-info btn-sm">
-                          Mark as Delivered
-                        </button>
+                        <Button type="submit" size="sm" variant={'outline'}>
+                          Delivered
+                        </Button>
                       </fetcher.Form>
                     )}
 
@@ -228,10 +303,9 @@ export default function Orders() {
                       <fetcher.Form method="delete">
                         <input type="hidden" name="orderId" value={order._id} />
                         <input type="hidden" name="action" value="delete" />
-                        <input type="hidden" name="token" value={token as string} />
-                        <button type="submit" className="btn btn-info btn-sm">
-                          Delete Order
-                        </button>
+                        <Button type="submit" size="sm" variant={'destructive'}>
+                          Delete
+                        </Button>
                       </fetcher.Form>
                     )}
 
@@ -240,53 +314,60 @@ export default function Orders() {
                       <fetcher.Form method="patch">
                         <input type="hidden" name="orderId" value={order._id} />
                         <input type="hidden" name="action" value="cancel" />
-                        <input type="hidden" name="token" value={token as string} />
-                        <button type="submit" className="btn btn-info btn-sm">
-                          Cancel Order
-                        </button>
+                        <Button type="submit" size="sm">
+                          Cancel
+                        </Button>
                       </fetcher.Form>
                     )}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={6} className="text-center py-4">
+                  No orders found.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
 
-      {/* Pagination Controls */}
-      <div className="flex justify-end gap-4 items-center mt-4">
-        <button
-          onClick={() => setPage(prev => Math.max(prev - 1, 1))}
-          className="btn btn-outline btn-sm"
-          disabled={page === 1}
-        >
-          Previous
-        </button>
-        <span className="text-secondary">
-          Page {page} of {totalPages}
-        </span>
-        <button
-          onClick={() => setPage(prev => (prev < totalPages ? prev + 1 : prev))}
-          className="btn btn-outline btn-sm"
-          disabled={page >= totalPages}
-        >
-          Next
-        </button>
-        <div>
-          <select
-            value={limit}
-            onChange={e => {
-              setPage(1)
-              setLimit(parseInt(e.target.value))
-            }}
-            className="select select-bordered btn-sm"
-          >
-            <option value="10">10 per page</option>
-            <option value="20">20 per page</option>
-            <option value="50">50 per page</option>
-          </select>
-        </div>
+          <TableFooter>
+            {/* pagination */}
+            <TableRow>
+              <TableHead>
+                <div className="flex items-center space-x-2">
+                  <Button size="sm" disabled={page <= 1} onClick={() => go({ page: page - 1 })}>
+                    <ArrowBigLeft />
+                  </Button>
+                  <span>
+                    Page <strong>{page}</strong> of <strong>{totalPages}</strong>
+                  </span>
+                  <Button
+                    size="sm"
+                    disabled={page >= totalPages}
+                    onClick={() => go({ page: page + 1 })}
+                  >
+                    <ArrowBigRight />
+                  </Button>
+                </div>
+              </TableHead>
+              <TableHead colSpan={4}>
+                <Select value={String(limit)} onValueChange={v => go({ limit: v, page: 1 })}>
+                  <SelectTrigger className="w-[80px]">
+                    <SelectValue placeholder="Limit" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {[6, 12, 18].map(n => (
+                      <SelectItem key={n} value={String(n)}>
+                        {n}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </TableHead>
+            </TableRow>
+          </TableFooter>
+        </Table>
       </div>
     </div>
   )
